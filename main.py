be6b0881c953c6fcc5b21ce7a61d865d370e6496
Vim Dela Rosa <vimdelarosa@gmail.com>
2020-12-06 15:27:02 +0800

    Test1

---
+++ main.py
@@ -0,0 +1,17 @@
+import turtle
+
+vim_turtle = turtle.Turtle()
+# This is a square
+def square():
+
+    vim_turtle.forward(100)
+    vim_turtle.right(90)
+    vim_turtle.forward(100)
+    vim_turtle.right(90)
+    vim_turtle.forward(100)
+    vim_turtle.right(90)
+    vim_turtle.forward(100)
+
+square()
+vim_turtle.forward(200)
+square()
